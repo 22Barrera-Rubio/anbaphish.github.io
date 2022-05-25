a1fad3ad91d8128c6fc008946bd97036ea6ed8d0
Phish <78632176+anbaphish@users.noreply.github.com>
2022-05-24 21:24:44 -0700

    added files

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function spam(){
+for (var x=0; x < 1; x-- ){
+prompt("Sne")
+}
+};
+    
+
---
+++ scrib.js
@@ -0,0 +1,86 @@
+/*
+potential improvements:
+Seperate drive function, search "putting a function into parameters javascript"
+This would allow the drive function to be utilized by many programs as the base starts of all of them are put into an array, then when JS is called in html it calls that array and the number of the function, thus running the function and cutting down on code as no more drives to prevent test crashing. 
+*/
+//spams boxes asking for input.
+function spam(){
+for (var x=0; x < 999999; x++ )
+prompt("Sne")
+};
+        /////// end ///////
+
+
+
+/////// does math and array stuff to crash window//
+function drive(){ //drivers function, done as it'd auto crash since the html calls for the function and open was already using large counting and stuff
+ // setTimeout(1); 
+  open();
+  
+}
+
+const c_array = [   // empty, just for dumping into
+];
+var Rand
+var RandT
+var RandTT
+var cook
+var CN
+var CNN
+var CNNN
+function open(){  //infinite enough loop, goes to first
+for (var x=0; x < 999999; x++ ){
+while(true){
+  prompt("Sne")
+  Opoint();
+  document.cookie = CNN + "=" + RandTT + Rand + RandT;
+  document.getElementById("CO").innerHTML = getCookie("CNN");
+}
+}
+};
+
+function Opoint(){ // does random number, goes to second
+ Rand = Math.floor(Math.random() * 999999);
+Tpoint();
+};
+
+function Tpoint(){ //does large multiplication, goes to third
+RandT = (Rand * 9999999 * Rand);
+TTpoint();
+  spam();
+};
+
+function TTpoint(){ //biggerer multiplication, dumps nums to array and spams html changes, hopefully so autogarbage process doesn't cleanup
+   RandTT = (RandT * Rand * Rand);
+
+  document.getElementById("spam").innerHTML = c_array;
+  c_array.push(Rand,RandT,RandTT); 
+  Opoint();
+};
+
+        /////////// end /////////
+
+// enter your own stuff
+var titleJ
+var headerJ
+var paraJ
+var para_
+function Pager(){
+let titleJ  = prompt("Enter your own title");
+document.getElementById("Title".innerHTMl) = titleJ;
+let headerJ = prompt("Enter your own header");
+document.getElementById("Header".innerHTMl) = headerJ;
+let paraJ  = prompt("Enter your own para");
+document.getElementById("ParagraphO".innerHTMl) = paraJ;
+let para_  = prompt("Enter your own header");
+document.getElementById("ParagraphT".innerHTMl) = Para_;
+  // to get it working since it doesn't first
+}
+
+
+
+
+
+
+
+
